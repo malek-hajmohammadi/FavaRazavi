081410f952e50db77d864ba01f2dd9955374af75
malek <malek.hajmohammadi@gmail.com>
2021-03-13 14:25:56 +0330

    139912231425

--- vTiger/fa_ir/Accounts.php
+++ vTiger/fa_ir/Accounts.php
@@ -48,3 +48,52 @@ $jsLanguageStrings = array(
 	'LBL_MASS_DELETE_CONFIRMATION' => 'حذف این سازمان(ها) فرصتها و پیش فاکتور هایش را حذف میکند. آیا مطمئن هستید؟',
 	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'آیا از حذف مطمئن هستید؟',
 );
+
+$languageStrings = array(
+    // Basic Strings
+    'Accounts' => 'Organizations',
+    'SINGLE_Accounts' => 'Organization',
+    'LBL_ADD_RECORD' => 'Add Organization',
+    'LBL_RECORDS_LIST' => 'Organization List',
+
+    // Blocks
+    'LBL_ACCOUNT_INFORMATION' => 'Organization Details',
+
+    // Mass Action
+    'LBL_SHOW_ACCOUNT_HIERARCHY' => 'Organization Hierarchy',
+
+    //Field Labels
+    'industry' => 'Industry',
+    'Account Name' => 'Organization Name',
+    'Account No' => 'Organization Number',
+    'Website' => 'Website',
+    'Ticker Symbol' => 'Ticker Symbol',
+    'Member Of' => 'Member Of',
+    'Employees' => 'Employees',
+    'Ownership' => 'Ownership',
+    'SIC Code' => 'SIC Code',
+    'Other Email' => 'Secondary Email',
+    'Other Phone' => 'Secondary Phone',
+    'Phone' => 'Primary Phone',
+    'Email' => 'Primary Email',
+    'Type' => 'Type',
+
+    //Added for existing picklist entries
+    'Analyst'=>'Analyst',
+    'Competitor'=>'Competitor',
+    'Customer'=>'Customer',
+    'Integrator'=>'Integrator',
+    'Investor'=>'Investor',
+    'Press'=>'Press',
+    'Prospect'=>'Prospect',
+    'Reseller'=>'Reseller',
+    'LBL_START_DATE' => 'Start Date',
+    'LBL_END_DATE' => 'End Date',
+
+    //Duplication error message
+    'LBL_DUPLICATES_EXIST' => 'Organization Name already exists',
+    'LBL_COPY_SHIPPING_ADDRESS' => 'Copy Shipping Address',
+    'LBL_COPY_BILLING_ADDRESS' => 'Copy Billing Address',
+    'LBL_IMAGE_INFORMATION' => 'Profile Picture',
+    'Organization Image' => 'Organization Image',
+);
