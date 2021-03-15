aca0b897207d63dd98e246fd925c82d816703e97
Malek Hajmohammadi <malek.hajmohammadi@gmail.com>
2021-03-15 22:45:58 +0330

    139912252245

---
+++ RavanEngineMHM/ajax_ارجاع محرمانه.php
@@ -0,0 +1,26 @@
+<?php
+class MainAjax
+{
+
+
+    public function main()
+    {
+        $sql="select * from `oa_doc_refer` WHERE RowID=1471278";
+        $db = WFPDOAdapter::getInstance();
+        $db->executeSelect($sql);
+
+        $row = $db->fetchAssoc();
+
+        return $row;
+    }
+
+
+
+
+}
+
+$mainAjax = new MainAjax();
+Response::getInstance()->response = $mainAjax->main();
+return $mainAjax;
+
+
---
+++ RavanEngineMHM/ajax_متن نامه ها.php
@@ -0,0 +1,31 @@
+<?php
+class MainAjax
+{
+
+
+    public function main()
+    {
+        $sql="select * from `oa_content` where RoleID=493 order by `RowID` desc limit 50";
+        $db = WFPDOAdapter::getInstance();
+        $db->executeSelect($sql);
+
+
+        $res = array();
+        while ($row = $db->fetchAssoc())
+        {
+            $res[] = $row;
+        };
+
+        return $res;
+    }
+
+
+
+
+}
+
+$mainAjax = new MainAjax();
+Response::getInstance()->response = $mainAjax->main();
+return $mainAjax;
+
+
